cc1d6773861d7a364a134524801eb4c384af49a4
Raj Sanjay Shah <rajsanjayshah@gmail.com>
2020-12-02 19:16:21 +0530

    Add files via upload

---
+++ optimizer.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+
+flags = tf.compat.v1.app.flags
+FLAGS = flags.FLAGS
+
+
+class OptimizerAE(object):
+    def __init__(self, preds, labels, pos_weight, norm):
+        preds_sub = preds
+        labels_sub = labels
+
+        self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)  # Adam Optimizer
+
+        self.opt_op = self.optimizer.minimize(self.cost)
+        self.grads_vars = self.optimizer.compute_gradients(self.cost)
+
+        self.correct_prediction = tf.equal(tf.cast(tf.greater_equal(tf.sigmoid(preds_sub), 0.5), tf.int32),
+                                           tf.cast(labels_sub, tf.int32))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+
+
+class OptimizerVAE(object):
+            
+    def __init__(self, preds, labels, model, num_nodes, pos_weight, norm):
+        from tensorflow.python.keras import backend as K
+        def loss_1st(y_true, y_pred):
+        	L = y_true
+        	Y = y_pred
+        	batch_size = tf.to_float(K.shape(L)[0])
+        	return 1e-6 * 2 * tf.linalg.trace(tf.matmul(tf.matmul(Y, L, transpose_b=True), Y)) / batch_size		
+		
+        preds_sub = preds
+        labels_sub = labels
+        #weights = tf.compat.v2.reduce_sum(pos_weight * labels_sub, axis=1)	
+        #self.cost = norm * tf.reduce_mean((weights* tf.nn.softmax_cross_entropy_with_logits(logits=preds_sub, labels=labels_sub, weights=pos_weight)))
+        self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)  # Adam Optimizer
+
+        # Latent loss
+        self.log_lik = self.cost
+        self.kl = (0.5 / num_nodes) * tf.reduce_mean(tf.reduce_sum(1 + 2 * model.z_log_std - tf.square(model.z_mean) -
+                                                                   tf.square(tf.exp(model.z_log_std)), 1))
+        self.cost -= self.kl
+        #self.cost+= loss_1st(model.z,model.z)
+        self.opt_op = self.optimizer.minimize(self.cost)
+        self.grads_vars = self.optimizer.compute_gradients(self.cost)
+
+        self.correct_prediction = tf.equal(tf.cast(tf.greater_equal(tf.sigmoid(preds_sub), 0.5), tf.int32),
+                                           tf.cast(labels_sub, tf.int32))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))       
+	
+        
+class OptimizerSDNE(object):
+    def __init__(self, preds, labels, pos_weight, norm):
+        preds_sub = preds
+        labels_sub = labels
+
+        self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)  # Adam Optimizer
+
+        self.opt_op = self.optimizer.minimize(self.cost)
+        self.grads_vars = self.optimizer.compute_gradients(self.cost)
+
+        self.correct_prediction = tf.equal(tf.cast(tf.greater_equal(tf.sigmoid(preds_sub), 0.5), tf.int32),
+                                           tf.cast(labels_sub, tf.int32))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
+        
